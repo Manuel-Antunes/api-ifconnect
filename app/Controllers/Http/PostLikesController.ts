9fed7ac5df2b6bc63c4fc0d355991d323eacff1a
Manuel Eduardo Nascimento Antunes <mena1@aluno.ifal.edu.br>
2021-08-28 15:38:48 -0300

    feat: likes controller

---
+++ app/Controllers/Http/PostLikesController.ts
@@ -0,0 +1,25 @@
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Post from 'App/Models/Post'
+
+export default class PostLikesController {
+  public async store({ params, auth, response }: HttpContextContract) {
+    try {
+      const post = await Post.findOrFail(params.post_id)
+      if (auth.user) {
+        await post.related('usersWhoLike').attach([auth.user.id])
+      }
+    } catch (error) {
+      return response.status(400).send({ error: { message: 'post not found' } })
+    }
+  }
+  public async destroy({ params, auth, response }: HttpContextContract) {
+    try {
+      const post = await Post.findOrFail(params.post_id)
+      if (auth.user) {
+        await post.related('usersWhoLike').detach([auth.user.id])
+      }
+    } catch (error) {
+      return response.status(400).send({ error: { message: 'post not found' } })
+    }
+  }
+}
