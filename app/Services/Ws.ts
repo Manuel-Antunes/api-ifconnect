a439e5d56c0f65cb5c22bba398f36be3fdffe98c
Manuel Eduardo Nascimento Antunes <mena1@aluno.ifal.edu.br>
2021-08-28 17:28:26 -0300

    chore: add cors config in ws server

--- app/Services/Ws.ts
+++ app/Services/Ws.ts
@@ -14,7 +14,11 @@ class Ws {
     }
 
     this.booted = true
-    this.io = new Server(AdonisServer.instance!)
+    this.io = new Server(AdonisServer.instance!, {
+      cors: {
+        origin: '*',
+      },
+    })
   }
 }
 
